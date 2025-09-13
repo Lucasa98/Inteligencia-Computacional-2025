e98a849ebe8a8672c739c5388466beedb2a2dcdd
lucasa98 <lusaurin@gmail.com>
2025-09-13 13:39:50 -0300

    Ej1

--- Práctica/src/Som1D.py
+++ Práctica/src/Som1D.py
@@ -1,25 +1,42 @@
 import numpy as np
 
 class Som1D:
-    def __init__(self, N: int, tasa_aprendizaje: float):
+    def __init__(self, N: int, vecindades: list[tuple[float]], tasa_aprendizaje: list[tuple[float]], epocas: list[int]):
         self.η = tasa_aprendizaje
         self.N = N
+        self.epocas = epocas
+        self.vecindades = vecindades
         self.rng = np.random.default_rng()
         self.history: list[np.ndarray[float]] = []
 
-    def entrenar(self, x: np.ndarray[float], max_epocas: int = 10):
-        # inicializar pesos
+    def entrenar(self, x: np.ndarray[float]):
+        W_etapas = []
+        # inicializar pesos (N x entradas)
         W = self.rng.uniform(-0.5, 0.5, (self.N, x.shape[1]))
-        entorno = 1
 
-        for _ in range(max_epocas):
-            for i in range(x.shape[0]):
-                # encontrar ganadora G = argmin(dist(W,x[i]))
-                Gi = np.argmin(np.sum(np.power(W - x[i],2), 1))
-                # definir vecinas
-                ini, fin = max(0,Gi-entorno), min(self.N, Gi+entorno+1)
-                # ajustar ganadora y vecinas
-                W[ini:fin] = W[ini:fin] + self.η * (x[i] - W[ini:fin])
+        # entrenamiento dividido en etapas
+        for j in range(len(self.epocas)):
+            ini_entorno = self.vecindades[j][0]
+            d_ent = (self.vecindades[j][1]-ini_entorno)/self.epocas[j]    # decaimiento de entorno
+            apr = self.η[j][0]                          # tasa de aprendizaje inicial
+            d_apr = (apr - self.η[j][1])/self.epocas[j] # decaimiento de la tasa de aprendizaje
+
+            for e in range(self.epocas[j]):
+                entorno = round(ini_entorno + e*d_ent)
+                for i in range(x.shape[0]):
+                    diff = x[i] - W
+                    # encontrar ganadora G = argmin(dist(W,x[i]))
+                    Gi = np.argmin(np.sum(np.power(diff,2), 1))
+                    # definir vecinas
+                    ini, fin = max(0,Gi-entorno), min(self.N, Gi+entorno+1)
+                    # ajustar ganadora y vecinas
+                    W[ini:fin] = W[ini:fin] + apr * diff[ini:fin]
+
+                # guardar historial
                 self.history.append(W.copy())
+                apr -= d_apr
+
+            W_etapas.append(W.copy())
 
         self.W = W
+        return W_etapas
